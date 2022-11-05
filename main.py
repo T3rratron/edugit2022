ef27305c7ee5e3dd3ecf720571d99ea71f4379ab
T3rratron <voynovdanny@gmail.com>
2022-11-05 13:31:42 +0300

    superfunction added

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+def superfunction():
+    print("cthulhu summoned")
+
 def main():
     print("initial print")
     print("secondary print")
     print("an another print")
+    superfunction()
+
 
 
 if __name__ == '__main__':
